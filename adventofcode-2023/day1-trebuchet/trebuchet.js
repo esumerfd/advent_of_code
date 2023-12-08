16667d496aa5c6f83c2166eb473245285bd834d6
Ed Sumerfield <esumerfield@stackct.com>
2023-12-07 19:57:59 -0500

    Trebuchet Final answer

--- adventofcode-2023/day1-trebuchet/trebuchet.js
+++ adventofcode-2023/day1-trebuchet/trebuchet.js
@@ -1,9 +1,17 @@
 // Node solution
 const fs = require('node:fs');
 
-function calculate(puzzleFile) {
+function calculateFile(puzzleFile) {
   let data = load(puzzleFile)
+  return calculate(data)
+}
+
+function calculate(data) {
   let sum = 0
+  calibrationLines(data, (line) => {
+    let decodedLine = firstLastDigit(line)
+    sum += decodedLine
+  })
   return sum
 }
 
@@ -11,11 +19,18 @@ function load(filename) {
   return fs.readFileSync(filename, 'utf8');
 }
 
-function firstLastDigit(calibrationLine) {
-  if (!calibrationLine) return ""
-  var digits = calibrationLine.replace(/[^\d]+/g, "")
-  if (digits) digits = digits[0] + digits.slice(-1)
-  return digits
+function calibrationLines(data, lineCallback) {
+  let lines = data.split('\n');
+  for(let i = 0;i < lines.length;i++){
+    if (lines[i].length > 0) lineCallback(lines[i])
+  }
 }
 
-module.exports = {  calculate, firstLastDigit  }
+function firstLastDigit(calibrationLine) {
+  if (!calibrationLine) return 0
+  var digits = calibrationLine.replace(/[^\d]+/g, "")
+  if (digits) digits = digits[0] + digits.slice(-1)
+  return Number(digits)
+}
+
+module.exports = { calculateFile, calculate, calibrationLines, firstLastDigit  }
--- adventofcode-2023/day1-trebuchet/trebuchet.spec.js
+++ adventofcode-2023/day1-trebuchet/trebuchet.spec.js
@@ -4,26 +4,49 @@ describe("trebuchet", function() {
   
   describe("final answer", function() {
     test("adds up numbers", function() {
-      let sum = trebuchet.calculate('puzzle_input')
+      let sum = trebuchet.calculateFile('puzzle_input')
 
-      expect(sum).toBe(0)
+      expect(sum).toBe(54304)
+    })
+
+    test.each`
+      expected | calibrationLine
+      ${12}       | ${"12"}
+      ${99}       | ${"aaaaa987cccccc789ddddd"}
+      ${11+22+33} | ${"a1a1a\nb2b2b\nc3c3c\n"}
+    `("expecting $expected from calibrationLine $calibrationLine", ({expected, calibrationLine}) => {
+
+      let sum = trebuchet.calculate(calibrationLine)
+
+      expect(sum).toBe(expected)
     })
   })
 
   describe("line numbers", function() {
     test("are first and last", function() {
-      expect(trebuchet.firstLastDigit("1abc2")).toBe("12")
-      expect(trebuchet.firstLastDigit("pqr3stu8vwx")).toBe("38")
-      expect(trebuchet.firstLastDigit("a1b2c3d4e5f")).toBe("15")
-      expect(trebuchet.firstLastDigit("treb7uchet")).toBe("77")
-      expect(trebuchet.firstLastDigit("789")).toBe("79")
+      expect(trebuchet.firstLastDigit("1abc2")).toBe(12)
+      expect(trebuchet.firstLastDigit("pqr3stu8vwx")).toBe(38)
+      expect(trebuchet.firstLastDigit("a1b2c3d4e5f")).toBe(15)
+      expect(trebuchet.firstLastDigit("treb7uchet")).toBe(77)
+      expect(trebuchet.firstLastDigit("789")).toBe(79)
     })
 
     test("nothing if bad input", function() {
-      expect(trebuchet.firstLastDigit("no digits")).toBe("")
-      expect(trebuchet.firstLastDigit("")).toBe("")
-      expect(trebuchet.firstLastDigit(null)).toBe("")
-      expect(trebuchet.firstLastDigit(undefined)).toBe("")
+      expect(trebuchet.firstLastDigit("no digits")).toBe(0)
+      expect(trebuchet.firstLastDigit("")).toBe(0)
+      expect(trebuchet.firstLastDigit(null)).toBe(0)
+      expect(trebuchet.firstLastDigit(undefined)).toBe(0)
+    })
+  })
+
+  describe("read data", function() {
+    test("one line at a time", function() {
+      let count = 0
+      let data = "line1\nline2\nline3\n"
+
+      trebuchet.calibrationLines(data, function (line) { count++ })
+
+      expect(count).toBe(3)
     })
   })
 })
