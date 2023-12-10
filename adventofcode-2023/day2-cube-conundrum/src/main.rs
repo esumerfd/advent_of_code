88ec62138347f38ccd413455700e0fd475718bbf
Ed Sumerfield <esumerfield@stackct.com>
2023-12-09 19:26:00 -0500

    Try using regex to parse Game line

--- adventofcode-2023/day2-cube-conundrum/src/main.rs
+++ adventofcode-2023/day2-cube-conundrum/src/main.rs
@@ -1,30 +1,28 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io;
+use std::fs;
+use std::str::FromStr;
+use regex::Regex;
 
-type Callback = fn(&str);
-
-fn main() {
-    start_puzzle("./puzzle_input")
+struct Game {
+    number: u32,
+    bag_draws: Vec<BagDraw>
 }
 
-fn start_puzzle(puzzle_filename: &str) {
-    games(puzzle_filename, bag_draws);
-}
+impl FromStr for Game {
+    type Err = std::num::ParseIntError;
 
-fn bag_draws(line: &str) {
-    // println!("{}", line);
-}
+    fn from_str(gameText: &str) -> Result<Self, Self::Err> {
 
-fn games(puzzle_filename: &str, bag_draws: Callback) {
-    if let Ok(lines) = read_lines(puzzle_filename) {
-        for line in lines {
-            if let Ok(ip) = line {
-                bag_draws(&ip);
-            }
-        }
+        // https://regex101.com/r/pP17Oo/1
+        let re = Regex::new(r"Game +(?P<number>\d+):((( *(?P<count>\d+) +(?P<color>blue|green|red)))[; ,]*)+").unwrap();
+
+        let caps = re.captures(gameText).unwrap();
+        println!(">>>> number {}", &caps["number"]);
+        println!(">>>> count {}", &caps["count"]);
+        println!(">>>> color {}", &caps["color"]);
+
+        Ok(Game { number: 0, bag_draws: Vec::new() })
     }
-
 }
 
 struct BagDraw {
@@ -33,22 +31,30 @@ struct BagDraw {
     red: u32,
 }
 
-fn parse_game(game: &str) -> Vec<BagDraw> {
-    vec![BagDraw{ blue: 1, green: 3, red: 2 }]
+fn main() {
+    start_puzzle("./puzzle_input")
 }
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = match File::open(filename) {
-        Ok(file) => file,
-        Err(err) => {
-            println!("Puzzle File: {}", err);
-            std::process::exit(1);
-        }
-    };
-    Ok(io::BufReader::new(file).lines())
+fn start_puzzle(puzzle_filename: &str) {
+    // Parse games, bag_draws
+    parse(puzzle_filename);
+    // Filter by max of each color
+    // sum game numbers of whats left
+
+}
+
+fn parse(puzzle_filename: &str) -> io::Result<Game> {
+    let contents = fs::read_to_string(puzzle_filename);
+    parse_games(&contents.unwrap());
+    return Ok(Game { number: 0, bag_draws: Vec::new()});
+}
+
+fn parse_games(games: &str) -> Vec<Game> {
+    return vec!(Game { number: 79, bag_draws: vec![BagDraw{ blue: 1, green: 3, red: 2 }] });
+}
+
+fn parse_game(game: &str) -> Game {
+    return Game::from_str(game).unwrap();
 }
 
 #[cfg(test)]
@@ -61,13 +67,12 @@ mod tests {
     }
 
     #[test]
-    fn should_parse_bad_draw() {
-        let bag_draw: Vec<BagDraw> = parse_game("Game 79: 3 green, 1 blue, 2 red; 8 green, 1 blue, 2 red; 2 blue, 1 red, 11 green");
+    fn should_parse_game() {
+        let game: Game = parse_game("Game 79: 3 green, 1 blue, 2 red; 8 green, 1 blue, 2 red; 2 blue, 1 red, 11 green");
 
-        println!("bad draw: blue {}, green {}, red {}", bag_draw[0].blue, bag_draw[0].green, bag_draw[0].red);
-
-        assert_eq!(1, bag_draw[0].blue);
-        assert_eq!(3, bag_draw[0].green);
-        assert_eq!(2, bag_draw[0].red);
+        assert_eq!(79, game.number);
+        assert_eq!(1, game.bag_draws[0].blue);
+        assert_eq!(3, game.bag_draws[0].green);
+        assert_eq!(2, game.bag_draws[0].red);
     }
 }
